1d2d2318d99b6cd4ef52cc247d2d1f4e9c4f0657
ms-online <27732357@qq.com>
2020-03-16 15:08:58 +0800

    lesson-6

--- script.js
+++ script.js
@@ -41,6 +41,44 @@ function pauseSong() {
   playBtn.querySelector("i.fas").classList.remove("fa-pause");
   audio.pause();
 }
+
+// prevSong
+function prevSong() {
+  songIndex--;
+  if (songIndex < 0) {
+    songIndex = songs.length - 1;
+  }
+  loadsong(songs[songIndex]);
+  playSong();
+}
+
+// nextSong
+function nextSong() {
+  songIndex++;
+  if (songIndex > songs.length - 1) {
+    songIndex = 0;
+  }
+  loadsong(songs[songIndex]);
+  playSong();
+}
+
+// 设置updateProgress
+function updateProgress(e) {
+  const { duration, currentTime } = e.srcElement;
+  const progressPercent = (currentTime / duration) * 100;
+  progress.style.width = `${progressPercent}%`;
+}
+
+// 设置setProgress
+function setProgress(e) {
+  const width = this.clientWidth;
+  //   console.log(width);
+  const clickX = e.offsetX;
+  //   console.log(clickX);
+  const duration = audio.duration;
+  audio.currentTime = (clickX / width) * duration;
+}
+
 // 事件监听
 playBtn.addEventListener("click", () => {
   const isPlaying = musicContainer.classList.contains("play");
@@ -51,3 +89,16 @@ playBtn.addEventListener("click", () => {
     playSong();
   }
 });
+
+// 切换歌曲
+prevBtn.addEventListener("click", prevSong);
+nextBtn.addEventListener("click", nextSong);
+
+// 更新进度条
+audio.addEventListener("timeupdate", updateProgress);
+
+// 点击进度条容器，更新歌曲播放
+progressContainer.addEventListener("click", setProgress);
+
+// 播放结束自动切换
+audio.addEventListener("ended", nextSong);
